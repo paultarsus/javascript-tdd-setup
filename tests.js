fdd2c341fea3b8f17fd4ca76edcc45181b5e76a8
Fabusuy Paul <paul_fabusuyi@yahoo.com>
2016-10-15 17:53:09 +0100

    Added two tests

--- tests.js
+++ tests.js
@@ -5,20 +5,16 @@ var assert = chai.assert;
 
 var lib = require('./lib/library.js');
 
-describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
+describe("input the number", function() {
+  it("should give the sum of the primes", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
+      lib.sumofprimes((5)==10)
     );
   });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
+  it("should return false for characters", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
+      lib.sumofprimes(("$%")==false)
     );
   });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
-    );
-  });
+   });
 });
